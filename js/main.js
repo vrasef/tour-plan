52805ea5101a7af5d0c3a67f7be5c7ddbf7a3564
vrasef <vrasef@mail.ru>
2022-04-01 09:44:23 +0300

    Добавили слайдер\свайпер

---
+++ js/main.js
@@ -0,0 +1,10 @@
+var swiper = new Swiper('.swiper', {
+  // Optional parameters
+  loop: true,
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.slider-button--next',
+    prevEl: '.slider-button--prev',
+  },
+});
