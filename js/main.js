91e27bcb9917ed047ba1410de5efac74d245fee7
vrasef <vrasef@mail.ru>
2022-04-06 10:05:19 +0300

    Добавил стилей для секции отзывы

--- js/main.js
+++ js/main.js
@@ -1,4 +1,15 @@
-var swiper = new Swiper('.hotel-slider', {
+var hotelSlider = new Swiper('.hotel-slider', {
+  // Optional parameters
+  loop: true,
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.hotel-slider__button--next',
+    prevEl: '.hotel-slider__button--prev',
+  },
+  effect: "coverflow",
+});
+var reviewsSlider = new Swiper('.reviews-slider', {
   // Optional parameters
   loop: true,
 
