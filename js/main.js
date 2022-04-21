0bba08e4b4dd18eb99a231ad0b0277e1a081b914
vrasef <vrasef@mail.ru>
2022-04-21 09:43:25 +0300

    Создал мобильное меню

--- js/main.js
+++ js/main.js
@@ -19,3 +19,11 @@ var reviewsSlider = new Swiper('.reviews-slider', {
     prevEl: '.reviews-slider__button--prev',
   },
 });
+
+var menuButton = document.querySelector(".menu-button");
+menuButton.addEventListener("click", function () {
+  console.log('click on menu button');
+  document
+    .querySelector(".navbar-bottom")
+    .classList.toggle("navbar-bottom--visible");
+});
