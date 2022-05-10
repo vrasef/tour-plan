62a347a942781b4175a7fe1b5d8b487e5ff5fbc6
vrasef <vrasef@mail.ru>
2022-05-10 15:48:45 +0300

    Добавил плагин валидации и поставил свои настройки

--- js/main.js
+++ js/main.js
@@ -44,4 +44,23 @@ $(document).ready(function () {
     modalOverlay.removeClass('modal__overlay--visible');
     modalDialog.removeClass('modal__dialog--visible');
   }
+  // Обработка форм
+  $('.modal__form').validate({
+    errorClass: "invalid",
+    messages: {
+    name: {
+      required: "Пожалуйтса, введите свое полное имя",
+      minlength: "Имя должно быть не короче 2 букв",
+    },
+    email: {
+      required: "Нам нужна ваша почта для контакта с вами",
+      minlength: "Почта должно быть не короче 8 букв",
+      email: "Your email address must be in the format of name@domain.com",
+    },
+    phone: {
+      required: "Телефон обязателен",
+      minlength: "Телефон должен быть не короче 5 цифр",
+    },
+  },
+  });
 });
